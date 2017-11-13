3e1615403039eed30ed4039412c2f68a3e782099
Clemens Schinnerl <cschinnerl@Clemenss-MacBook-Pro.local>
2017-11-13 18:48:49 +0100

    renamed app.sh to officemix.sh and added clean up after adding

--- README.md
+++ README.md
@@ -7,3 +7,26 @@ Create a playlist with the most heard tracks of your co-workers, friends, etc.
 ### Prerequisites
 
 - [JQ](https://stedolan.github.io/jq/)
+
+### How to use
+#### Configuration
+1. Create an folder named *auth*
+2. You will need to register a new Application at https://developer.spotify.com/my-applications
+3. Take note of the Client ID and Client Secret
+4. For the 'Redirect URI' you will need to use http://localhost:8082/ (or any other port)
+5. Create a new file `data.conf` and replace `id` and `secret` with the values from Steps 1
+
+```
+id="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+secret="bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+callback="localhost"
+port=8082
+user="cccccccccccccccccccccccccccccccc"
+playlist="dddddddddddddddddddddddddddddddd"
+```
+
+## Usage
+1. clone git
+2. `cd officemix && chmod +x officemix.sh`
+3. `./officemix.sh -add #name` to add a new user'
+4. `./officemix.sh`
--- officemix.sh
+++ officemix.sh
@@ -7,7 +7,7 @@ oauth2 () {
   if [ ! -e ./$1 ]
   then
     redirect_uri="http%3A%2F%2F$callback%3A$port%2F"
-    scopes=$(echo "playlist-read-private playlist-modify-private user-read-private user-top-read" | tr ' ' '%' | sed s/%/%20/g)
+    scopes=$(echo "playlist-modify-public playlist-modify-private user-top-read" | tr ' ' '%' | sed s/%/%20/g)
     auth_endpoint="https://accounts.spotify.com/authorize/?response_type=code&client_id=$id&redirect_uri=$redirect_uri&scope=$scopes"
     echo "Please visit: $auth_endpoint"
     response=$(echo -e "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin:*\r\n" | nc -l "${port}")
@@ -23,7 +23,7 @@ oauth2 () {
 
 toptracks() {
   response=$(curl -X GET "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=$2&offset=0" -H "Accept: application/json" -H "Authorization: Bearer $1" | jq -r '.items | .[].uri')
-  echo "$response" >> tracks.txt
+  echo "$response" >> $3
 }
 addtoplaylist() {
   source 'data.conf'
@@ -31,27 +31,56 @@ addtoplaylist() {
   while IFS='' read -r line || [[ -n "$line" ]]; do
     encodedtrack=$(php -r "echo urlencode(\"$line\");")
     tracks="${tracks}$encodedtrack,"
-  done < tracks.txt
+  done < $2
 
   tracks="${tracks%?}"
 
   addresponse=$(curl -X POST "https://api.spotify.com/v1/users/$user/playlists/$playlist/tracks?position=0&uris=$tracks" -H "Accept: application/json" -H "Authorization: Bearer $1")
 }
 
-tracksfile='tracks.txt'
-maxsongs=100
-usercount=$(ls -d auth/*oauth* | wc -l)
-limit=$((maxsongs / usercount))
+clean() {
+  rm -rf $1
+}
 
-FILES=auth/*
-> tracks.txt
-for f in $FILES
-do
-  authfile=$(basename "$f")
-  token=''
-  oauth2 $authfile
-  toptracks $token $limit
-  addtoplaylist $token
-done
+folder="auth"
+mkdir -p auth
+
+if [ ! -d $folder ]; then
+  mkdir -p $folder
+fi
+
+if [ $# -eq 2 ]
+then 
+  if [ "$1" == "-add" ]
+  then
+    oauth2 "$folder/oauth.$2"
+  else
+    echo "unkown parameter"
+  fi
+else
+  if [ $# -eq 0 ]
+  then 
+    tracksfile='tracks.txt'
+    maxsongs=50
+    usercount=$(ls -d auth/*oauth* | wc -l)
+    if [ $usercount -ne 0 ]
+    then
+      limit=$((maxsongs / usercount))
+
+      FILES=auth/*
+      > $tracksfile
+      for f in "$folder/*"
+      do
+        authfile=$(basename "$f")
+        token=''
+        oauth2 "$folder/$authfile"
+        toptracks $token $limit $tracksfile
+        addtoplaylist $token $tracksfile
+      done
+      clean $tracksfile
+    fi
+  fi
+fi
+
 
 
