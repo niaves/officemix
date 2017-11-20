6905a7baefde3046ca3a031653392854a2f3e47a
Clemens Schinnerl <cschinnerl@Clemenss-MacBook-Pro.local>
2017-11-20 18:47:29 +0100

    fixed top tracks reading and playlist deleting

--- officemix.sh
+++ officemix.sh
@@ -14,19 +14,28 @@ oauth2 () {
     code=$(echo "${response}" | grep GET | cut -d ' ' -f 2 | cut -d '=' -f 2)
     response=$(curl -s https://accounts.spotify.com/api/token -H "Content-Type:application/x-www-form-urlencoded" -H "Authorization: Basic $b64" -d "grant_type=authorization_code&code=$code&redirect_uri=$redirect_uri")
     token=$(echo "${response}" | jq -r '.access_token')
-    echo "${response}" | jq -r '.refresh_token' > $1
+    refresh=$(echo "${response}" | jq -r '.refresh_token')
+    echo "$2;$refresh" > $1
   else
-    refresh_token=$(cat $1)
+    line=$(head -n 1 $1)
+    arr=$(echo $line | tr ";" "\n")
+
+    authuser="$(cut -d';' -f1 <<< $line)"
+    refresh_token="$(cut -d';' -f2 <<< $line)"
+
     token=$(curl -s -X POST https://accounts.spotify.com/api/token -H "Content-Type:application/x-www-form-urlencoded" -H "Authorization: Basic $b64" -d "grant_type=refresh_token&refresh_token=$refresh_token" | jq -r '.access_token')
   fi
 }
 
 toptracks() {
-  response=$(curl -X GET "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=$2&offset=0" -H "Accept: application/json" -H "Authorization: Bearer $1" | jq -r '.items | .[].uri')
+  response=$(curl -X GET "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=$2" -H "Accept: application/json" -H "Authorization: Bearer $1" | jq -r '.items | .[].uri')
   echo "$response" >> $3
 }
 addtoplaylist() {
   source 'data.conf'
+
+  oauth2 $1
+
   tracks=''
   while IFS='' read -r line || [[ -n "$line" ]]; do
     encodedtrack=$(php -r "echo urlencode(\"$line\");")
@@ -35,7 +44,7 @@ addtoplaylist() {
 
   tracks="${tracks%?}"
 
-  addresponse=$(curl -X POST "https://api.spotify.com/v1/users/$user/playlists/$playlist/tracks?position=0&uris=$tracks" -H "Accept: application/json" -H "Authorization: Bearer $1")
+  addresponse=$(curl -X POST "https://api.spotify.com/v1/users/$authuser/playlists/$playlist/tracks?position=0&uris=$tracks" -H "Accept: application/json" -H "Authorization: Bearer $token")
 }
 
 clean() {
@@ -44,27 +53,40 @@ clean() {
 
 clearplaylist() {
   source "data.conf"
-  for file in "$1/*" 
-  do
-    oauth2 $file
-    break 1
-  done
+  maxallowed=100
 
-  snapshot=$(curl -X GET "https://api.spotify.com/v1/users/$user/playlists/$playlist" -H "Authorization: Bearer $token" | jq -r '.snapshot_id')
+  oauth2 $1
 
-  position='['
-  for (( c=0; c<$2; c++ ))
-  do  
-     position="$position$c,"
-  done
-  position="${position%?}]"
-  
-  delete=$(curl -X DELETE "https://api.spotify.com/v1/users/$user/playlists/$playlist/tracks" -H "Authorization: Bearer $token" -H "Content-Type: application/json" --data "{\"positions\":$position,\"snapshot_id\":\"$snapshot\"}")
+  total=$(curl -X GET "https://api.spotify.com/v1/users/$user/playlists/$playlist" -H "Authorization: Bearer $token" | jq -r '.tracks.total')
 
-  echo delete
+  tmp=$total
+
+  if [ "$total" -ne 0 ]
+  then  
+    snapshot=$(curl -X GET "https://api.spotify.com/v1/users/$user/playlists/$playlist" -H "Authorization: Bearer $token" | jq -r '.snapshot_id')
+        
+    while [ $tmp -gt 0 ]
+    do
+        count=$tmp
+        if [ $tmp -gt $maxallowed ]
+        then
+          count=maxallowed
+        fi
+        position='['
+
+        for (( c=0; c<$count; c++ ))
+        do  
+           position="$position$c,"
+        done
+        position="${position%?}]"
+        snapshot=$(curl -X DELETE "https://api.spotify.com/v1/users/$user/playlists/$playlist/tracks" -H "Authorization: Bearer $token" -H "Content-Type: application/json" --data "{\"positions\":$position,\"snapshot_id\":\"$snapshot\"}" | jq -r '.snapshot_id')
+        tmp=$(echo $(( tmp - count )))
+    done
+  fi
 }
 
 folder="auth"
+mastertoken="master.token"
 mkdir -p auth
 
 if [ ! -d $folder ]; then
@@ -73,37 +95,39 @@ fi
 
 if [ $# -eq 2 ]
 then 
+  if [ "$1" == "-init" ]
+  then
+    oauth2 $mastertoken $2
+  else
+      echo "unkown parameter"
+  fi
+fi
+if [ $# -eq 3 ]
+then 
   if [ "$1" == "-add" ]
   then
-    oauth2 "$folder/oauth.$2"
+    oauth2 "$folder/oauth.$2" $3
   else
-    echo "unkown parameter"
+      echo "unkown parameter"
   fi
 else
   if [ $# -eq 0 ]
   then
-    maxsongs=50
-    clearplaylist $folder $maxsongs
+    maxsongs=75
+    clearplaylist $mastertoken $maxsongs
     tracksfile='tracks.txt'
-    usercount=$(ls -d auth/*oauth* | wc -l)
+    > $tracksfile
 
-    if [ $usercount -ne 0 ]
-    then
-      limit=$((maxsongs / usercount))
-      FILES=auth/*
-      > $tracksfile
-
-      for f in "$folder/*"
-      do
-        authfile=$(basename "$f")
-        token=''
-        oauth2 "$folder/$authfile"
-        toptracks $token $limit $tracksfile
-        addtoplaylist $token $tracksfile
-      done
-
-      clean $tracksfile
-    fi
+    for f in "$folder/*"
+    do
+      authfile=$(basename "$f")
+      token=''
+      authuser=''
+      oauth2 "$folder/$authfile"
+      toptracks $token $maxsongs $tracksfile
+      addtoplaylist $mastertoken $tracksfile
+    done
+    clean $tracksfile
   fi
 fi
 
